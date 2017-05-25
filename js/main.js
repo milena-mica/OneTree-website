830acc6b5b39869a3b8336ddfbf073c30e3515bf
Milena Trajkovic <trajkovic.milena.mica@gmail.com>
2017-05-25 23:09:27 +0200

    Making the navigation bar stick to tope, when scrolling.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-    
+$(document).ready(function() {
+				 
+	$(window).scroll(function() {
+					
+		if ($(window).scrollTop() > 970) {		
+		   $('.navbar').addClass('navbar-fixed-top');		
+	 }	
+		if ($(window).scrollTop() < 971) {		
+		   $('.navbar').removeClass('navbar-fixed-top');		
+	 }				
+		
+					
+ })			 
+				 
+				 
+				 
+				 
+});    
