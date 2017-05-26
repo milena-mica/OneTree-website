16b3e0e0de533c07296f33670aff5883c636394b
Milena Trajkovic <trajkovic.milena.mica@gmail.com>
2017-05-26 23:48:02 +0200

    Update home page and add some new features

--- js/main.js
+++ js/main.js
@@ -7,12 +7,61 @@ $(document).ready(function() {
 	 }	
 		if ($(window).scrollTop() < 971) {		
 		   $('.navbar').removeClass('navbar-fixed-top');		
-	 }				
+	 }					
+
+	})						 
+});
+
+
+$(".navbar-toggleable-md ul li a[href^='#']").on('click',function(e) {
+
+  e.preventDefault();
+	
+	var hash = this.hash;
+	
+	$('html,body').animate ({
+	   scrollTop: $(hash).offset().top
+	 }, 300 , function(){
 		
-					
- })			 
-				 
-				 
-				 
-				 
-});    
+	  window.location.hash = hash;
+		
+	});
+	
+});
+
+
+$("#section-parallax .col-sm-8 a[href^='#']").on('click',function(e) {
+
+  e.preventDefault();
+	
+	var hash = this.hash;
+	
+	$('html,body').animate ({
+	   scrollTop: $(hash).offset().top
+	 }, 300 , function(){
+		
+	  window.location.hash = hash;
+		
+	});
+	
+});
+
+
+$(".footer .scroll-to-top-button a[href^='#']").on('click',function(e) {
+
+  e.preventDefault();
+	
+	var hash = this.hash;
+	
+	$('html,body').animate ({
+	   scrollTop: $(hash).offset().top
+	 }, 300 , function(){
+		
+	  window.location.hash = hash;
+		
+	});
+	
+});
+
+
+
